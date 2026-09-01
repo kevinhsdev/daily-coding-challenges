75e8ed9fca2b629675f31427b1e5413109fe6a1a
kevinhsdev <kevinhs07@outlook.com>
2026-09-01 16:50:12 -0300

    upload content from last commit

--- arrays/001-basic/001-basic.java
+++ arrays/001-basic/001-basic.java
@@ -1,13 +1,19 @@
-// Beecrowd 1001 - Extremamente Basico
-// Leia dois numeros e imprima a soma
+// Beecrowd 1001 - Extremely Basic
+// Le dois numeros inteiros e imprime a soma
 
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println("X = " + (a + b));
+
+        int x = a + b;
+
+        System.out.println("X = " + x);
     }
+
 }
